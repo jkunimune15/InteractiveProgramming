55e2ce8ec35daa179e841c5d24f16d2c2a0cb118
jkunimune15 <jkunimune15@punahou.edu>
2016-03-02 11:47:46 -0500

    Figured out volume control in wavTest.py

--- wavTest.py
+++ wavTest.py
@@ -2,10 +2,16 @@ import pygame
 
 import time
 
+import math
+
 pygame.init()
 
-pygame.mixer.music.load("test.wav")
+pygame.mixer.music.load("music.wav")
 
 pygame.mixer.music.play()
 
+i = 0
 while True:
+	time.sleep(0.1)
+	i = i+.1
+	pygame.mixer.music.set_volume(math.pow(math.sin(i),2))
